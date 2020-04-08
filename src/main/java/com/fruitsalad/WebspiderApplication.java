8dca495787f65d95eb68749fb53966f33f1f6a2f
wzh <wzhlink@163.com>
2020-04-08 17:18:31 +0800

    init

---
+++ src/main/java/com/fruitsalad/WebspiderApplication.java
@@ -0,0 +1,21 @@
+package com.fruitsalad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * All rights Reserved, Designed By ""
+ *
+ * @version V1.0
+ * @package com.fruitsalad
+ * @description:
+ * @author: wangzhihao
+ * @date: 2020/4/8
+ * @copyright: 2020 "" Inc. All rights reserved.
+ */
+@SpringBootApplication
+public class WebspiderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebspiderApplication.class,args);
+    }
+}
